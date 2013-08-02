a55b1e2c3b06737254ec97cdfc237a634ba659df
Luis Ramón López <lrlopez@gmail.com>
2013-08-02 21:47:09 +0200

    feat(multitenancy): Multitenancy initial support. Minor fixes.
    Soporte inicial de múltiples organizaciones. Otros ajustes menores.

--- public/index.php
+++ public/index.php
@@ -23,32 +23,7 @@ session_name('ATICAID');
 session_cache_limiter(false);
 session_start();
 
-// Leer configuración global
-$config = array();
-
-$data = ORM::for_table('configuration')->where_not_null('content_type')->
-        where_null('organization_id')->find_array();
-
-// Leer datos de la organización
-if (isset($_SESSION['organization_id'])) {
-    $organization =
-            ORM::for_table('organization')->
-            find_one($_SESSION['organization_id'])->as_array();
-    
-    // configuración local de la organización
-    $data = array_merge($data, ORM::for_table('configuration')->
-            where_equal('organization_id', $_SESSION['organization_id'])->
-            where_not_null('content_type')->
-            find_array());
-} else {
-    $organization = NULL;
-}
-
-foreach($data as $item) {
-    $config[$item['item_id']] = $item['content'];
-}
-
-// Prepare app
+// Preparar aplicación
 $app = new \Slim\Slim(array(
     'templates.path' => '../templates',
     'log.level' => 4,
@@ -59,7 +34,7 @@ $app = new \Slim\Slim(array(
             ))
         ));
 
-// Prepare view
+// Preparar vistas
 $view = $app->view(new \Slim\Views\Twig());
 $view->parserOptions = array(
     'charset' => 'utf-8',
@@ -72,14 +47,74 @@ $view->parserExtensions = array(
     new \Slim\Views\TwigExtension(),
 );
 $twig = $view->getInstance();
+
+
+// Leer configuración global
+$config = array();
+
+$data = ORM::for_table('configuration')->where_not_null('content_type')->
+        where_null('organization_id')->find_array();
+
+// Leer datos de la organización
+$organization = NULL;
+if (isset($_SESSION['organization_id'])) {
+    $organization =
+            ORM::for_table('organization')->
+            find_one($_SESSION['organization_id'])->as_array();
+} else {
+    if (1 == ORM::for_table('organization')->count()) {
+        $organization = ORM::for_table('organization')->find_one()->as_array();
+        $_SESSION['organization_id'] = $organization['id'];
+    }
+}
+
+if (NULL != $organization) {
+    // configuración local de la organización
+    $data = array_merge($data, ORM::for_table('configuration')->
+            where_equal('organization_id', $_SESSION['organization_id'])->
+            where_not_null('content_type')->
+            find_array());
+}
+
+foreach($data as $item) {
+    $config[$item['item_id']] = $item['content'];
+}
+
 $twig->addGlobal('organization', $organization);
 $twig->addGlobal('config', $config);
-// Define routes
+
+// Definir rutas
 $app->get('/', function () use ($app) {
-    $app->redirect('/inicio');
+    if (!isset($_SESSION['organization_id'])) {
+        $app->redirect('organizacion');
+    }
+    $app->redirect('inicio');
+});
+
+$app->get('/organizacion', function () use ($app) {
+    $breadcrumb = NULL;
+    $organizations = ORM::for_table('organization')->
+            order_by_asc('display_name')->find_array();
+    $app->render('organizacion.html.twig', array('navigation' => $breadcrumb,
+        'organizations' => $organizations));
+})->name('organizacion');
+
+$app->post('/organizacion', function () use ($app) {
+    $organization_nr = ORM::for_table('organization')->
+            where('id',$_POST['organization_id'])->count();
+    if (1 == $organization_nr) {
+        $_SESSION['organization_id'] = $_POST['organization_id'];
+        $app->redirect('/inicio');
+    }
+    else {
+        $app->redirect('/organizacion');
+    }
 });
 
 $app->get('/inicio(/:id)', function ($id = '') use ($app) {
+    if (!isset($_SESSION['organization_id'])) {
+        $app->redirect('organizacion');
+    }
     $breadcrumb = array(
         array('display_name' => 'Portada', 'target' => '#'));
     $app->render('inicio.html.twig', array(
@@ -87,17 +122,25 @@ $app->get('/inicio(/:id)', function ($id = '') use ($app) {
 })->name('inicio');
 
 $app->get('/entrar(/:id)', function ($id = '') use ($app) {
+    if (!isset($_SESSION['organization_id'])) {
+        $app->redirect('organizacion');
+    }    
     $breadcrumb = array(array('display_name' => 'Acceder', 'target' => '#'));
     $app->render('entrar.html.twig', array('navigation' => $breadcrumb));
 })->name('entrar');
 
 $app->post('/entrar(/:id)', function ($id = '') use ($app) {
+    if (!isset($_SESSION['organization_id'])) {
+        $app->redirect('organizacion');
+    }
     $breadcrumb = array(array('display_name' => 'Dentro', 'target' => '#'));
     $app->render('base.html.twig', array('navigation' => $breadcrumb));
 });
 
 $app->get('/bienvenida', function () use ($app) {
-    $instance = array('display_name' => 'I.E.S. Oretania');
+    if (!isset($_SESSION['person_id'])) {
+        $app->redirect('inicio');
+    }    
     $breadcrumb = array(array('display_name' => 'Primer acceso', 'target' => '#'));
     $app->render('bienvenida.html.twig', array('navigation' => $breadcrumb));
 })->name('bienvenida');
