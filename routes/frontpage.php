eab80cf0d28baf39f935121d72fe12717403344b
Luis Ramón López <lrlopez@gmail.com>
2013-08-27 16:26:35 +0200

    feat(frontpage): Initial frontpage grouping support
    - Soporte inicial de agrupamientos en portada

--- routes/frontpage.php
+++ routes/frontpage.php
@@ -16,24 +16,68 @@
   You should have received a copy of the GNU Affero General Public License
   along with this program.  If not, see [http://www.gnu.org/licenses/]. */
 
-$app->get('/(portada)', function () use ($app, $user) {
+$app->get('/(portada)(/:id)', function ($id = NULL) use ($app, $user) {
     if (!isset($_SESSION['organization_id'])) {
         $app->redirect($app->urlFor('organization'));
     }
     $breadcrumb = array(
         array('display_name' => 'Portada', 'target' => '#')
     );
-    $sidebar = array();
-
-    array_push($sidebar, array(
-        array('caption' => 'Secciones', 'icon' => 'list'),
-        array('caption' => 'Portada', 'active' => true, 'target' => $app->urlFor('frontpage')),
-        array('caption' => 'Plan de centro', 'target' => '#'),
-        array('caption' => 'Criterios de evaluación', 'target' => '#'),
-        array('caption' => 'Programaciones didácticas', 'target' => '#')
-    ));
-
+    
+    $matchedGrouping = array();
+    
+    $sidebar = getGroupings($_SESSION['organization_id'], $app, $id, $matchedGrouping);
+    
     $app->render('frontpage.html.twig', array(
         'navigation' => $breadcrumb, 'search' => true, 'sidebar' => $sidebar,
         'user' => $user));
 })->name('frontpage');
+
+function getGroupings($orgId, $app, $id, &$matchedGrouping) {
+    $return = array();
+    $currentData = array();
+    $currentGrouping = NULL;
+    $match = false;
+
+    $data = ORM::for_table('grouping')->
+            order_by_asc('grouping_left')->
+            where('organization_id', $orgId)->
+            where_gt('grouping_level', 0)->
+            find_many();
+
+    foreach ($data as $grouping) {
+        if ($grouping['grouping_level'] == 1) {
+            if ($currentGrouping != NULL) {
+                array_unshift($currentData,
+                        array(
+                            'caption' => $currentGrouping['display_name']
+                        ));
+                $return[] = $currentData;
+            }
+            $currentData = array();
+            $currentGrouping = $grouping;
+            $match = false;
+        }
+        else {
+            $localMatch = ($id == $grouping['id']);
+            $currentData[] = array(
+                'caption' => $grouping['display_name'],
+                'active' => $localMatch,
+                'target' => $app->urlFor('frontpage', array('id' => $grouping['id']))
+            );
+            if ($localMatch) {
+                $matchedGrouping = $grouping;
+            }
+            $match = $match || $localMatch;
+        }
+    }
+    if ($currentGrouping != NULL) {
+          array_unshift($currentData,
+                  array(
+                      'caption' => $currentGrouping['display_name']
+                  ));
+          $return[] = $currentData;
+    }
+
+    return $return;
+}
