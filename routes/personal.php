71b357aa30a866fb8dfbdf89bceacb427e6bb532
Luis Ramón López <lrlopez@gmail.com>
2013-09-15 20:15:34 +0200

    chore(indentation): Indentation fix
    - Ajustada la indentación

--- routes/personal.php
+++ routes/personal.php
@@ -17,157 +17,157 @@
   along with this program.  If not, see [http://www.gnu.org/licenses/]. */
 
 $app->map('/personal/:section/:id', function ($section, $id) use ($app, $user, $organization, $preferences) {
-            if (!$user) {
-                $app->redirect($app->urlFor('login'));
+    if (!$user) {
+        $app->redirect($app->urlFor('login'));
+    }
+
+    // ¿se busca la información del usuario activo?
+    $itsMe = ($id == NULL) || ($id == $user['id']);
+
+    // si es así, asignar sus datos
+    if ($itsMe) {
+        $id = $user['id'];
+        $userData = $user;
+    } else {
+        // cargar los datos del usuario indicado como parámetro
+        $userData = getUserById($id, $organization['id']);
+        if (!$userData) {
+            // ¿no existe en la organización? Salir de aquí
+            $app->redirect($app->urlFor('frontpage'));
+        }
+    }
+
+    // comprobar si se están cambiando datos
+    if (isset($_POST['savepersonal']) && ($user['is_admin'] || $itsMe)) {
+        $local = getUserObjectById($id);
+        if (isset($_POST['displayname'])) {
+            $local->set('display_name', $_POST['displayname']);
+        }
+        if (isset($_POST['firstname'])) {
+            $local->set('first_name', $_POST['firstname']);
+        }
+        if (isset($_POST['lastname'])) {
+            $local->set('last_name', $_POST['lastname']);
+        }
+        if (isset($_POST['initials'])) {
+            $local->set('initials', $_POST['initials']);
+        }
+        if (isset($_POST['gender'])) {
+            $local->set('gender', $_POST['gender']);
+        }
+        if (isset($_POST['email'])) {
+            $local->set('email', $_POST['email']);
+        }
+        if (isset($_POST['notify'])) {
+            $local->set('email_enabled', $_POST['notify']);
+        }
+        if ($user['is_admin']) {
+            if (isset($_POST['username'])) {
+                $local->set('user_name', $_POST['username']);
             }
+            if (isset($_POST['description'])) {
+                $local->set('description', strlen($_POST['description']) > 0 ? $_POST['description'] : NULL);
+            }
+            if (isset($_POST['active']) && (isset($_POST['localadmin']))) {
+                setPersonIsActiveAndLocalAdmin($_POST['active'], $_POST['localadmin'], $id, $organization['id']);
+            }
+            // permitir cambiar opción de administrador global si ya lo somos
+            // y el usuario activo no somos nosotros (para evitar accidentes)
+            if ($user['is_global_administrator'] && isset($_POST['globaladmin']) && !$itsMe) {
+                $local->set('is_global_administrator', $_POST['globaladmin']);
+            }
+        }
+        if ($local->save()) {
+            $app->flash('save_ok', 'ok');
+        } else {
+            $app->flash('save_error', 'error');
+        }
+        $app->redirect($app->urlFor('personal', array('id' => $id, 'section' => $section)));
+    }
 
-            // ¿se busca la información del usuario activo?
-            $itsMe = ($id == NULL) || ($id == $user['id']);
-
-            // si es así, asignar sus datos
-            if ($itsMe) {
-                $id = $user['id'];
-                $userData = $user;
+    // cambio de contraseña
+    if (isset($_POST['savepassword']) && isset($_POST['password1']) &&
+            isset($_POST['password2']) && ($user['is_admin'] || $itsMe)) {
+        $changeOk = false;
+        if (!$user['is_admin']) {
+            if (!isset($_POST['oldpassword']) || !checkUserPassword($user['id'], $_POST['oldpassword'], $preferences['salt'])) {
+                $app->flashNow('save_error', 'oldpassword');
             } else {
-                // cargar los datos del usuario indicado como parámetro
-                $userData = getUserById($id, $organization['id']);
-                if (!$userData) {
-                    // ¿no existe en la organización? Salir de aquí
-                    $app->redirect($app->urlFor('frontpage'));
-                }
+                $changeOk = true;
             }
-
-            // comprobar si se están cambiando datos
-            if (isset($_POST['savepersonal']) && ($user['is_admin'] || $itsMe)) {
-                $local = getUserObjectById($id);
-                if (isset($_POST['displayname'])) {
-                    $local->set('display_name', $_POST['displayname']);
-                }
-                if (isset($_POST['firstname'])) {
-                    $local->set('first_name', $_POST['firstname']);
-                }
-                if (isset($_POST['lastname'])) {
-                    $local->set('last_name', $_POST['lastname']);
-                }
-                if (isset($_POST['initials'])) {
-                    $local->set('initials', $_POST['initials']);
-                }
-                if (isset($_POST['gender'])) {
-                    $local->set('gender', $_POST['gender']);
-                }
-                if (isset($_POST['email'])) {
-                    $local->set('email', $_POST['email']);
-                }
-                if (isset($_POST['notify'])) {
-                    $local->set('email_enabled', $_POST['notify']);
-                }
-                if ($user['is_admin']) {
-                    if (isset($_POST['username'])) {
-                        $local->set('user_name', $_POST['username']);
-                    }
-                    if (isset($_POST['description'])) {
-                        $local->set('description', strlen($_POST['description']) > 0 ? $_POST['description'] : NULL);
-                    }
-                    if (isset($_POST['active']) && (isset($_POST['localadmin']))) {
-                        setPersonIsActiveAndLocalAdmin($_POST['active'], $_POST['localadmin'], $id, $organization['id']);
-                    }
-                    // permitir cambiar opción de administrador global si ya lo somos
-                    // y el usuario activo no somos nosotros (para evitar accidentes)
-                    if ($user['is_global_administrator'] && isset($_POST['globaladmin']) && !$itsMe) {
-                        $local->set('is_global_administrator', $_POST['globaladmin']);
-                    }
-                }
-                if ($local->save()) {
-                    $app->flash('save_ok', 'ok');
-                } else {
-                    $app->flash('save_error', 'error');
-                }
-                $app->redirect($app->urlFor('personal', array('id' => $id, 'section' => $section)));
-            }
-
-            // cambio de contraseña
-            if (isset($_POST['savepassword']) && isset($_POST['password1']) &&
-                    isset($_POST['password2']) && ($user['is_admin'] || $itsMe)) {
+        } else {
+            $changeOk = true;
+        }
+        if ($changeOk) {
+            if ($_POST['password1'] !== $_POST['password2']) {
+                $app->flashNow('save_error', 'passwordmatch');
                 $changeOk = false;
-                if (!$user['is_admin']) {
-                    if (!isset($_POST['oldpassword']) || !checkUserPassword($user['id'], $_POST['oldpassword'], $preferences['salt'])) {
-                        $app->flashNow('save_error', 'oldpassword');
-                    } else {
-                        $changeOk = true;
-                    }
-                } else {
-                    $changeOk = true;
-                }
-                if ($changeOk) {
-                    if ($_POST['password1'] !== $_POST['password2']) {
-                        $app->flashNow('save_error', 'passwordmatch');
-                        $changeOk = false;
-                    } elseif (strlen($_POST['password1']) < 6) {
-                        $app->flashNow('save_error', 'passwordlength');
-                        $changeOk = false;
-                    }
-                }
-                if ($changeOk) {
-                    $local = getUserObjectById($id);
-                    $local->set('password', sha1($preferences['salt'] . $_POST['password1']));
-                    $local->save();
-                    $app->flash('save_ok', 'ok');
-                    $app->redirect($app->urlFor('personal', array('id' => $id, 'section' => 0)));
-                }
+            } elseif (strlen($_POST['password1']) < 6) {
+                $app->flashNow('save_error', 'passwordlength');
+                $changeOk = false;
             }
+        }
+        if ($changeOk) {
+            $local = getUserObjectById($id);
+            $local->set('password', sha1($preferences['salt'] . $_POST['password1']));
+            $local->save();
+            $app->flash('save_ok', 'ok');
+            $app->redirect($app->urlFor('personal', array('id' => $id, 'section' => 0)));
+        }
+    }
 
-            // menú lateral de secciones
-            $menu = array(
-                array('caption' => ($itsMe ? 'Mis datos' : $userData['display_name']), 'icon' => 'user')
-            );
+    // menú lateral de secciones
+    $menu = array(
+        array('caption' => ($itsMe ? 'Mis datos' : $userData['display_name']), 'icon' => 'user')
+    );
 
-            // las secciones vienen en este array
-            $options = array(
-                0 => array('caption' => 'Personal', 'template' => 'personal'),
-                1 => array('caption' => 'Perfiles', 'template' => 'profiles')/* ,
-                      2 => array('caption' => 'Envíos realizados', 'template' => 'personal') */
-            );
+    // las secciones vienen en este array
+    $options = array(
+        0 => array('caption' => 'Personal', 'template' => 'personal'),
+        1 => array('caption' => 'Perfiles', 'template' => 'profiles')/* ,
+              2 => array('caption' => 'Envíos realizados', 'template' => 'personal') */
+    );
 
-            // si el usuario es él mismo o es administrador, permitir cambiar la contraseña
-            // y ver el informe de actividad
-            if ($itsMe || $user['is_admin']) {
-                //$options[3] = array('caption' => 'Registro de actividad', 'template' => 'personal');
-                $options[4] = array('caption' => 'Cambiar contraseña', 'template' => 'password');
-            }
+    // si el usuario es él mismo o es administrador, permitir cambiar la contraseña
+    // y ver el informe de actividad
+    if ($itsMe || $user['is_admin']) {
+        //$options[3] = array('caption' => 'Registro de actividad', 'template' => 'personal');
+        $options[4] = array('caption' => 'Cambiar contraseña', 'template' => 'password');
+    }
 
-            // comprobar que la sección existe
-            if (!isset($options[$section])) {
-                $app->redirect($app->urlFor('frontpage'));
-            }
+    // comprobar que la sección existe
+    if (!isset($options[$section])) {
+        $app->redirect($app->urlFor('frontpage'));
+    }
 
-            // generar menú
-            foreach ($options as $key => $i) {
-                $menu[] = array('caption' => $i['caption'], 'active' => ($section == $key), 'target' => $app->urlFor('personal', array('id' => $id, 'section' => $key)));
-            }
+    // generar menú
+    foreach ($options as $key => $i) {
+        $menu[] = array('caption' => $i['caption'], 'active' => ($section == $key), 'target' => $app->urlFor('personal', array('id' => $id, 'section' => $key)));
+    }
 
-            $sidebar = array(
-                $menu
-            );
+    $sidebar = array(
+        $menu
+    );
 
-            // lista perfiles del usuario
-            $profiles = getProfilesByUser($id);
+    // lista perfiles del usuario
+    $profiles = getProfilesByUser($id);
 
-            // generar barra de navegación
-            $breadcrumb = array(
-                array('display_name' => 'Usuarios', 'target' => $app->urlFor('personal', array('id' => $user['id'], 'section' => 0))),
-                array('display_name' => $userData['display_name'], 'target' => $app->urlFor('personal', array('id' => $id, 'section' => 0))),
-                array('display_name' => $options[$section]['caption'])
-            );
-            // lanzar plantilla
-            $app->render($options[$section]['template'] . '.html.twig', array(
-                'navigation' => $breadcrumb,
-                'sidebar' => $sidebar,
-                'url' => $app->request()->getPathInfo(),
-                'userData' => $userData,
-                'profiles' => $profiles,
-                'local' => $itsMe
-            ));
-        })->name('personal')->via('GET', 'POST');
+    // generar barra de navegación
+    $breadcrumb = array(
+        array('display_name' => 'Usuarios', 'target' => $app->urlFor('personal', array('id' => $user['id'], 'section' => 0))),
+        array('display_name' => $userData['display_name'], 'target' => $app->urlFor('personal', array('id' => $id, 'section' => 0))),
+        array('display_name' => $options[$section]['caption'])
+    );
+    // lanzar plantilla
+    $app->render($options[$section]['template'] . '.html.twig', array(
+        'navigation' => $breadcrumb,
+        'sidebar' => $sidebar,
+        'url' => $app->request()->getPathInfo(),
+        'userData' => $userData,
+        'profiles' => $profiles,
+        'local' => $itsMe
+    ));
+})->name('personal')->via('GET', 'POST');
 
 function getUserById($personId, $orgId) {
     return ORM::for_table('person')->
