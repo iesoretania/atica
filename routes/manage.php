08938ec8288f73b5c2c24d17417aafedd6a393a8
Luis Ramón López <lrlopez@gmail.com>
2013-10-03 12:52:25 +0200

    feat(delivery): Creation date can be updated now
    - La fecha de creación puede ser actualizada ahora

--- routes/manage.php
+++ routes/manage.php
@@ -86,6 +86,9 @@ $app->map('/modificar/:folderid/:id', function ($folderId, $id) use ($app, $user
     if (isset($_POST['save'])) {
         $delivery->set('display_name', $_POST['displayname']);
         $delivery->set('description', strlen($_POST['displayname']) > 0 ? $_POST['displayname'] : null);
+        if (isset($_POST['creation_year'])) {
+            $delivery->set('creation_date', $_POST['creation_year'] . '/'. $_POST['creation_month'] . '/' . $_POST['creation_day'] . ' ' .$_POST['creation_hour'] . ':' . $_POST['creation_minute']);
+        }
         $delivery->save();
         $app->flash('save_ok', 'ok');
         $app->redirect($app->request()->getPathInfo());
